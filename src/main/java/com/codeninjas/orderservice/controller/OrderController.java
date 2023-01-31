c9f1274e1880963dde2b8c09b6a899516e1e0c17
EdiTeare <edomteare96@gmail.com>
2023-01-30 16:51:01 -0800

    worked on controller class,order service CRUD , exception and validation

--- src/main/java/com/codeninjas/orderservice/controller/OrderController.java
+++ src/main/java/com/codeninjas/orderservice/controller/OrderController.java
@@ -1,12 +1,50 @@
 package com.codeninjas.orderservice.controller;
 
+import com.codeninjas.orderservice.domain.Order;
+import com.codeninjas.orderservice.exception.OrderServiceException;
+import com.codeninjas.orderservice.service.OrderService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/orders")
 @RequiredArgsConstructor
 public class OrderController {
 
+    private final OrderService orderService;
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderService.getAllOrder();
+    }
+
+    @PostMapping
+    public Order createOrder(@Valid @RequestBody Order order) {
+        return orderService.addOrder(order);
+    }
+
+    @GetMapping("{id}")
+    public Order getOrderById(@PathVariable String id) throws OrderServiceException {
+        return orderService.getOrderById(id);
+
+    }
+
+    @DeleteMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> deleteById(@PathVariable String id) throws OrderServiceException {
+        orderService.deleteOrder(id);
+        return new ResponseEntity<String>(HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> updateById(@PathVariable String id, @Valid @RequestBody Order order) throws OrderServiceException {
+        orderService.updateOrder(id, order);
+        return new ResponseEntity<String>(HttpStatus.OK);
+    }
 }
--- src/main/java/com/codeninjas/orderservice/service/OrderServiceImpl.java
+++ src/main/java/com/codeninjas/orderservice/service/OrderServiceImpl.java
@@ -1,10 +1,52 @@
 package com.codeninjas.orderservice.service;
 
+import com.codeninjas.orderservice.domain.Order;
+import com.codeninjas.orderservice.exception.OrderServiceException;
+import com.codeninjas.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
+    private final OrderRepository orderRepository;
+
+
+    @Override
+    public List<Order> getAllOrder() {
+
+        return orderRepository.findAll();
+    }
+
+    @Override
+    public Order addOrder(Order Order) {
+
+        return orderRepository.save(Order);
+    }
+
+    @Override
+    public Order updateOrder(String id, Order order) throws OrderServiceException {
+        Order newOrder = orderRepository.findById(id).orElseThrow(() -> new OrderServiceException("Order not found"));
+        newOrder.setOrderId(id);
+        newOrder.setOrderStatus(order.getOrderStatus());
+        newOrder.setAddressId(order.getAddressId());
+        newOrder.setQuantity(order.getQuantity());
+        orderRepository.save(newOrder);
+        return newOrder;
+    }
+
+    @Override
+    public void deleteOrder(String id) throws OrderServiceException {
+        orderRepository.findById(id).orElseThrow(() -> new OrderServiceException("Order not found"));
+        orderRepository.deleteById(id);
+
+    }
+
+    @Override
+    public Order getOrderById(String id) throws OrderServiceException {
+        return orderRepository.findById(id).orElseThrow(() -> new OrderServiceException("Order not found"));
+    }
 }
